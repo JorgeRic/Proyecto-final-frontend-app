c2658db19ce1ed5c4f80abdd3d263dc1d59c5394
Jorge <jorrillop@hotmail.com>
2019-08-25 22:26:55 +0200

    last commit2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,12 +6,12 @@ function Footer() {
   
       <img className="logo" src ="cityzennegro.jpg" alt="logo" />
     <div className="agrademientos">
-      <h4>Agradecimientos:</h4>
+      {/* <h4>Agradecimientos:</h4>
       <ul>
         <li><p>A mis cuñados por mantenerme y apoyarme constantemente</p></li>
         <li><p>A mis profesores por su paciencia</p></li>
         <li><p>A mis compañeros por su ayuda y solidaridad</p></li>
-      </ul>
+      </ul> */}
       </div>
 
     </div>
